eee015802cd522cb647a62b2bcfe2341ee2559d6
AntonioMoccia <moccia.ant@gmail.com>
2026-04-28 20:37:49 +0200

    fix: add 'use client' to History, ImageReveal, Irpinia components

--- app/components/History.tsx
+++ app/components/History.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import ImageReveal from './ImageReveal'
 import AnimatedText from '@/app/components/AnimatedText'
--- app/components/Irpinia.tsx
+++ app/components/Irpinia.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useGSAP } from '@gsap/react'
 import { useRef } from 'react'
 import gsap from 'gsap'
